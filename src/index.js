b0d5ebeab9657c5585a6a17be92158f68eb6876e
shawnwhy <shawnyudesign@gmail.com>
2022-04-24 18:41:14 -0400

    sdsdsds

--- src/index.js
+++ src/index.js
@@ -4,29 +4,51 @@ const load = require('load-asset');
 import style from './main.css'
 // const { ContextReplacementPlugin } = require('webpack');
 let fpsInterval;
-let dotsmove;
 let then;
 let startTime;
 let now
 let elapsed;
+let agents = [];
+let dotsmove = 'off'
+let canvas
+let movieInterval
+let context
+let typeContext
+let typeCanvas
+let video
+let cell = 5
+let generateFilter
+let shape='circles'
+let mousePos
+let inputStrings = []
+let glyphs = '_= /'.split('');
+let videoWidth;
+let videoHeight;
+let videoChoice = "pedler"
+// glyphs.push("Ukrain")
 
-const settings = {
-	dimensions: [ 1080, 1080 ]
+
+const getGlyph = (v) => {
+	
+	return random.pick(glyphs);
 };
 
-let agentPushed = 0 
-let manager;
+function getMousePos(canvas, evt) {
+console.log("gettingmouse pos")
+  var rect = canvas.getBoundingClientRect();
+	console.log(rect)
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+}
 
-let text = 'A';
-let fontSize = 1200;
-let fontFamily = 'serif';
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-
-let agents = [];
-
-
-
+function startAnimating(fps) {
+    fpsInterval = 1000 / fps;
+    then = Date.now();
+    startTime = then;
+    tick();
+}
 class Agent {
 	constructor(x, y, fillStyle, cell) {
 		// this.pos = new Vector(x, y);
@@ -34,9 +56,42 @@ class Agent {
 		this.y=y;
 		this.radius=cell/2;
 		this.fillStyle=fillStyle
-		this.vel = {x:random.range(-50, 50), y:random.range(-50, 50)};
+		this.vel = {x:random.range(-.5, .5), y:random.range(-.5, .5)};
 
 	}
+
+
+getDistance(v) {
+		const dx = this.x - v.x;
+		const dy = this.y - v.y;
+		return Math.sqrt(dx * dx + dy * dy);
+}
+
+moveBit(){
+	this.x += this.vel.x;
+	this.y += this.vel.y;
+	switch(shape){
+case 'circles':
+this.draw(context)
+break;
+case 'squares':
+this.drawSquare(context)
+break;
+case 'rumbus':
+this.drawRumbus(context)
+break;
+case 'words':
+context.fillStyle = 'black';
+context.textBaseline = 'middle';
+context.textAlign = 'center';
+this.drawText(context)
+}
+
+this.bounce(1000, 1000)
+}
+
+
+
 
 	bounce(width, height) {
 		if (this.x <= 0 || this.x >= width)  this.vel.x *= -1;
@@ -53,29 +108,93 @@ class Agent {
 
 		context.fillStyle = this.fillStyle
 		context.translate(this.x, this.y);
-	    context.translate(this.radius * 0.5, this.radius * 0.5);
+	    context.translate(this.radius, this.radius);
 
 		context.beginPath();
 		context.arc(0, 0, this.radius, 0, Math.PI * 2);
 		context.fill();
 		context.restore();
 	}
+	drawSquare(context) {
+		context.save();
+
+		context.fillStyle = this.fillStyle
+		context.translate(this.x, this.y);
+	    // context.translate(this.radius, this.radius);
+
+		context.beginPath();
+
+		context.fillRect(0,0,this.radius*2, this.radius*2);
+		context.fill();
+		context.restore();
+	}
+	drawRumbus(context) {
+		context.save();
+
+		context.fillStyle = this.fillStyle
+		context.translate(this.x, this.y);
+		context.rotate(Math.PI*.25);
+
+	    // context.translate(this.radius, this.radius);
+
+		context.beginPath();
+
+		context.fillRect(0,0,this.radius, this.radius);
+
+
+
+		context.fill();
+
+		context.restore();
+	}
+	drawText(context) {
+
+
+		const glyph = getGlyph(200);
+		fontSize = this.radius * 2.4;
+
+		typeContext.fillStyle = 'black';
+	
+
+			context.font = `${this.radius * 2}px ${fontFamily}`;
+			if (Math.random() < 0.1) context.font = `${this.radius * 6}px ${fontFamily}`;
+			typeContext.textBaseline = 'top';
+
+		context.fillStyle = this.fillStyle
+
+			context.save();
+			context.translate(this.x, this.y);
+			context.translate(this.radius * 0.5, this.radius * 0.5);
+
+			// context.fillRect(0, 0, cell, cell);
+
+			context.fillText(glyph, 0, 0);
+			
+			context.restore();
+
+		
+	}
 }
 
 
 
-let typeCanvas=document.getElementById('canvas1');
-let canvas=document.getElementById('canvas2');
-const context = canvas.getContext('2d');
+let agentPushed = 0 
+let fontSize = 1200;
+let fontFamily = 'serif';
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+typeCanvas=document.getElementById('canvas1');
+canvas=document.getElementById('canvas2');
+context = canvas.getContext('2d');
 
 // console.log(typeCanvas)
-const typeContext = typeCanvas.getContext('2d');
-var video  = document.getElementById('video');
-const cell = 40;
+typeContext = typeCanvas.getContext('2d');
+video  = document.getElementById('video');
 var checkvideo = setInterval(() => {
 	if ( video.readyState === 4 ) {
-var videoWidth = video.videoWidth;
-var videoHeight = video.videoHeight;
+videoWidth = video.videoWidth;
+videoHeight = video.videoHeight;
 // console.log(videoHeight)
 var typeCanvasHeight = videoHeight/cell;
 var typeCanvasWidth = videoWidth/cell
@@ -92,11 +211,15 @@ clearInterval(checkvideo)
 
 
 
-video.addEventListener('play', function () {
+//main funciton
+generateFilter= function(video){
 	// console.log('mousedown')
-	var moveieInterval = setInterval(() => {
-	dotsmove=  'off'
-	// agents = [];
+	clearInterval(movieInterval)
+	 movieInterval = setInterval(() => {
+	context.clearRect(0, 0, canvas.width, canvas.height);
+
+	dotsmove= 'off'
+	agents = [];
 
     var $this = this; //cache
 	const width= typeCanvas.width;
@@ -107,10 +230,10 @@ video.addEventListener('play', function () {
 	const rows = height;
 	const numCells = cols * rows
   
-        typeContext.drawImage($this, 0, 0, width, height);
+        typeContext.drawImage(video, 0, 0, width, height);
 		var typeData = typeContext.getImageData(0, 0, width, height).data;
 		// console.log(typeData)
-		context.fillStyle = 'white';
+		context.fillStyle = 'black';
 		context.fillRect(0, 0, width, height);
 
 		context.textBaseline = 'middle';
@@ -143,38 +266,90 @@ video.addEventListener('play', function () {
 	
 				agents.forEach(agent=>{
 
-			agent.draw(context);})
+			switch(shape){
+case 'circles':
+agent.draw(context)
+break;
+case 'squares':
+agent.drawSquare(context)
+break;
+case 'rumbus':
+agent.drawRumbus(context)
+break;
+case 'words':
+context.textBaseline = 'middle';
+context.textAlign = 'center';
+agent.drawText(context)
+}
+
+
+
+
+})
 	}	
 
 }, 500);
 
+
+if(canvas){
+canvas.addEventListener('mousedown', canvasClick);
+canvas.addEventListener('mousemove', (e)=>{
+console.log('moisemove')
+movePixles(e)
+})
+}
+}
+
+
+
+video.addEventListener('play', function () {
+
+generateFilter(video);
+
 })
 
-dotsmove = 'off'
+
+
+})
 const canvasClick = ()=>{
+clearInterval(movieInterval)
 console.log('canvas click')
+if(dotsmove=='off'){
 dotsmove ='on'
 }
-canvas.addEventListener('mousedown', canvasClick);
-// console.log(agents)
-function startAnimating(fps) {
-    fpsInterval = 1000 / fps;
-    then = Date.now();
-    startTime = then;
-    tick();
+else{
+dotsmove="off"
+generateFilter(video);
+}
 }
 
+
 const tick = () =>{
+
 if(agents.length>0&&dotsmove=='on'){
-context.clearRect(0, 0, canvas.width, canvas.height);
 for(let i = 0; i<agents.length;i++){
 agents[i].update()
+switch(shape){
+case 'circles':
 agents[i].draw(context)
-// agents[i].bounce(1000, 1000)
+break;
+case 'squares':
+agents[i].drawSquare(context)
+break;
+case 'rumbus':
+agents[i].drawRumbus(context)
+break;
+case 'words':
+context.fillStyle = 'black';
+context.textBaseline = 'middle';
+context.textAlign = 'center';
+agents[i].drawText(context)
+}
+
+agents[i].bounce(videoWidth, videoHeight)
 }
 }
      window.requestAnimationFrame(tick)
-
     now = Date.now();
     elapsed = now - then;
 
@@ -187,10 +362,97 @@ agents[i].draw(context)
         then = now - (elapsed % fpsInterval);   
 }
 }
-
 startAnimating(5)
 
-})
-// start();
+
+const selectSizes = document.getElementById('sizeSelect')
+//select sizes function
+const changeSize= function(){
+let sizeValue = parseInt(selectSizes.value);
+console.log(sizeValue);
+cell=sizeValue
+	if ( video.readyState === 4 ) {
+var videoWidth = video.videoWidth;
+var videoHeight = video.videoHeight;
+// console.log(videoHeight)
+var typeCanvasHeight = videoHeight/cell;
+var typeCanvasWidth = videoWidth/cell
+// console.log(typeCanvasHeight)
+// console.log(typeCanvasWidth)
+typeCanvas.width = typeCanvasWidth;
+typeCanvas.height = typeCanvasHeight;
+canvas.height = videoHeight;
+canvas.width= videoWidth;
+clearInterval(movieInterval);
+generateFilter(video)
+	}
+}
+selectSizes.addEventListener('change', ()=>{changeSize()})
 
 
+const selectFilter = document.getElementById('filterSelect')
+selectFilter.addEventListener('change', ()=>{changeFilter()})
+
+
+function changeFilter(){
+console.log(selectFilter.value)
+shape = selectFilter.value;
+}
+
+
+
+
+const loadVideo= function(url){
+
+video  = document.getElementById('video');
+switch(videoChoice){
+case "troika":
+video.src = "./troika.mp4"
+break;
+
+case "pedler":
+video.src="./koro.mp4"
+}
+// console.log(video)
+
+
+}
+
+loadVideo()
+// selectVideos.addEventListener('change', loadVideo())
+
+const wordsInputForm = document.getElementById('inputWordsForm')
+const wordsInput = document.getElementById('inputWords')
+
+wordsInputForm.addEventListener('submit',(e)=>{
+e.preventDefault();
+e.stopPropagation();
+
+addWords()})
+
+function addWords(){
+
+
+var word = wordsInput.value;
+console.log(wordsInput.value)
+if(word.length<20){
+inputStrings.push(word)
+glyphs=glyphs.concat(inputStrings)
+console.log(glyphs)
+
+};
+}
+
+const movePixles=(e)=>{
+mousePos = getMousePos(canvas, e)
+
+for (let i = 0; i < agents.length; i++) {
+	const agent = agents[i];
+const dist = agent.getDistance(mousePos);
+	if (dist > 200) continue;
+		agent.moveBit()
+		}	
+
+
+
+}
